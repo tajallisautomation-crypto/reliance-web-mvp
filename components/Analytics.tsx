17bfec812181607b9e80ced066a6e27b51ac4853
tajallisautomation-crypto <tajallisautomation@gmail.com>
2026-02-18 16:38:00 +0500

    Update Analytics.tsx

--- components/Analytics.tsx
+++ components/Analytics.tsx
@@ -1,9 +1,22 @@
+// components/Analytics.tsx
 export default function Analytics() {
   return (
-    <script
-      defer
-      data-domain="reliance.tajallis.com.pk"
-      src="https://plausible.io/js/script.js"
-    />
+    <>
+      {/* Plausible */}
+      <script
+        defer
+        data-domain="reliance.tajallis.com.pk"
+        src="https://plausible.io/js/script.js"
+      />
+
+      {/* Helper: window.plausible(eventName, {props}) */}
+      <script
+        dangerouslySetInnerHTML={{
+          __html: `
+            window.plausible = window.plausible || function(){(window.plausible.q = window.plausible.q || []).push(arguments)};
+          `,
+        }}
+      />
+    </>
   );
 }
