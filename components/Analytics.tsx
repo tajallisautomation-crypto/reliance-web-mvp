5699a9cab6df15978ca90ed92cf3c336c53b9df5
tajallisautomation-crypto <tajallisautomation@gmail.com>
2026-02-18 15:59:20 +0500

    Create Analytics.tsx

---
+++ components/Analytics.tsx
@@ -0,0 +1,9 @@
+export default function Analytics() {
+  return (
+    <script
+      defer
+      data-domain="reliance.tajallis.com.pk"
+      src="https://plausible.io/js/script.js"
+    />
+  );
+}
