b8da374fd0809954b12e2698f9ccbc3b5b7998b3
tajallisautomation-crypto <tajallisautomation@gmail.com>
2026-02-19 22:01:25 +0500

    Create FloatingWhatsApp.tsx

---
+++ FloatingWhatsApp.tsx
@@ -0,0 +1,13 @@
+"use client";
+
+export default function FloatingWhatsApp() {
+  return (
+    <a
+      href="https://wa.me/923354266238"
+      target="_blank"
+      className="fixed bottom-6 right-6 bg-green-600 hover:bg-green-700 text-white p-4 rounded-full shadow-2xl transition"
+    >
+      💬
+    </a>
+  );
+}
