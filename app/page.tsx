12866d00e14a6eccf92a9720a40583231cb1bf7f
tajallisautomation-crypto <tajallisautomation@gmail.com>
2026-02-21 05:15:16 +0500

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,20 +1,4 @@
-import Hero from "./components/Hero";
-import LogoBannerRail from "@/components/LogoBannerRail";
-import PromoBanners from "@/components/PromoBanners";
-import CatalogClient from "./components/CatalogClient";
-import { fetchProducts } from "../lib/products";
 
-export default async function Page() {
-  const products = await fetchProducts();
-
-  return (
-    <main>
-      <Hero />
-      <LogoBannerRail />
-      <PromoBanners />
-      <section id="catalogue" className="mx-auto max-w-7xl px-4 pb-20">
-        <CatalogClient products={products} whatsappNumberDigits="923354266238" />
-      </section>
-    </main>
-  );
+export default function Home() {
+  return <div>Test</div>;
 }
