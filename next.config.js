c61cb7fb08b16c09bc8253ff7e4e3c9d0167f3cb
tajallisautomation-crypto <tajallisautomation@gmail.com>
2026-02-23 06:37:37 +0500

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -73,3 +73,15 @@ module.exports = {
     ],
   },
 };
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      { protocol: "https", hostname: "**" },
+      { protocol: "http", hostname: "**" }
+    ],
+    unoptimized: true
+  }
+};
+
+module.exports = nextConfig;
